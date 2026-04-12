c7dd9a1a75a3b27d21934571e4b1ebf7fa7abab2
tomas-zahradnicek <tomas.zahradnicek.hradec@gmail.com>
2026-04-12 03:07:00 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["exceljs"],
+  },
+  test: {
+    environment: "node",
+    include: ["src/**/*.test.ts"],
   },
 });
